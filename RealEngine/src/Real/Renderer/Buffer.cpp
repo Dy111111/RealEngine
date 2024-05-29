1c474862f6696643b7b5282f3e8c206feddb4327
lisaidy <lisaidy@163.com>
2024-05-29 20:33:04 +0800

    update

---
+++ RealEngine/src/Real/Renderer/Buffer.cpp
@@ -0,0 +1,34 @@
+#include "repch.h"
+#include "Buffer.h"
+
+#include "Renderer.h"
+
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Real {
+
+	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    RE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLVertexBuffer(vertices, size);
+		}
+
+		RE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    RE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLIndexBuffer(indices, size);
+		}
+
+		RE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
